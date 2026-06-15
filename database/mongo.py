c8577789c1e8a32b5cc884cd8c4502fd42b4bbfc
Abhishek Saxena <abhisheksaxena10sgricbly@gmail.com>
2026-06-15 10:22:09 +0530

    Updated project

--- database/mongo.py
+++ database/mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-"mongodb://localhost:27017/"
+"mongodb+srv://abhishek20:abhishek123123@cluster0.85usmfn.mongodb.net/?appName=Cluster0"
 )
 
 db = client["resume_analyzer"]
